1b467d6a9878a952903523f25c6a3a26804f2106
lfss <28lfss@gmail.com>
2026-06-04 16:53:12 -0300

    Add application configuration files

---
+++ src/main/java/com/lfss/libixel/LibixelApplication.java
@@ -0,0 +1,13 @@
+package com.lfss.libixel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibixelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibixelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfss/libixel/LibixelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfss.libixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibixelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
